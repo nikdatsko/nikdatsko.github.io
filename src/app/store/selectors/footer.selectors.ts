cf186b0c850f1c5a2beb652cc78ac4178ff20ee6
Mykyta Datsko <mykyta_datsko@epam.com>
2023-12-05 17:27:09 -0800

    delete store

--- src/app/store/selectors/footer.selectors.ts
+++
@@ -1,8 +0,0 @@
-import { getRouterState } from './../reducers/index';
-import { createSelector } from '@ngrx/store';
-import { getRouterUrl } from './router.selectors';
-
-export const getFooterWord = createSelector(
-  getRouterUrl,
-  url => (url === '/portfolio' ? 'portfolio' : 'resumé')
-);
--- src/app/store/selectors/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './router.selectors';
-export * from './footer.selectors';
