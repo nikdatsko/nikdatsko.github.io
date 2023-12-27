9ce7fe666b62195eeb80a70dc5816e91b29beefd
Mykyta Datsko <mykyta_datsko@epam.com>
2023-12-27 11:20:33 -0800

    ngx-build-plus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: 'portfolio',
-    loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+    component: HomeComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
+    CommonModule,
     SharedModule,
     FormsModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    HomeModule,
   ],
   bootstrap: [AppComponent]
 })
