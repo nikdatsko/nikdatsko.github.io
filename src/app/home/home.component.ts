76ce1dc7d2b000b6add2d412bf5c99a0019f44ba
Mykyta Datsko <mykyta_datsko@epam.com>
2023-12-27 19:22:50 -0800

    pdf link

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,13 @@ import { DataTypes } from './home.enum';
 export class HomeComponent {
   readonly experienceStream: Observable<Place[]> = this.homeService.getData(DataTypes.experience);
   readonly skillsStream: Observable<{[key: string]: string[]}> = this.homeService.getData(DataTypes.skills);
+  readonly pdfLink = 'https://www.sejda.com/html-to-pdf?' +
+    'save-link=' + encodeURIComponent("https://nikdatsko.github.io/") +
+    '&pageOrientation=portrait' +
+    '&pageMargin=40px' +
+    '&usePrintMedia=true' +
+    '&delay=2' +
+    '&pageSize=letter';
   disableReorder = () => 0;
 
   constructor(private homeService: HomeService) {}
